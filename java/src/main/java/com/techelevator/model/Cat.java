10346f5c126f4a178f2897a093783f585cac143a
Student <student@Techs-MacBook-Air.local>
2020-12-04 16:50:23 -0500

    Cat Class Created

---
+++ java/src/main/java/com/techelevator/model/Cat.java
@@ -0,0 +1,62 @@
+package com.techelevator.model;
+
+public class Cat {
+	
+	private String name;
+	private int age;
+	private String skills;
+	private String hairLength;
+	private int priorExperienceMonths;
+	private String previousJobs;
+	private String description;
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getSkills() {
+		return skills;
+	}
+	public void setSkills(String skills) {
+		this.skills = skills;
+	}
+	public String getHairLength() {
+		return hairLength;
+	}
+	public void setHairLength(String hairLength) {
+		this.hairLength = hairLength;
+	}
+	public int getPriorExperienceMonths() {
+		return priorExperienceMonths;
+	}
+	public void setPriorExperienceMonths(int priorExperienceMonths) {
+		this.priorExperienceMonths = priorExperienceMonths;
+	}
+	public String getPreviousJobs() {
+		return previousJobs;
+	}
+	public void setPreviousJobs(String previousJobs) {
+		this.previousJobs = previousJobs;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+
+
+
+
+
+
+}
