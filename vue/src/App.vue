4da7d3e9974c9b89de2aecbd7ff289eb99767fcb
Elaina <elainarusetsky@gmail.com>
2020-12-07 13:16:26 -0500

    commit

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,9 +1,22 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
+       <header-main></header-main>
+    
+      <!-- <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp; -->
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     </div>
     <router-view />
+   
   </div>
 </template>
+
+<script>
+import HeaderMain from '../src/components/header.vue';
+
+export default{
+components: {
+  HeaderMain
+}
+};
+</script>
---
+++ vue/src/components/header.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="header-main">
+     <!-- <svg src="/assets/small-logo.svg" />  -->
+    <img src="/assets/small-logo.jpg"/>
+    <smallLogo/>
+  </div>
+</template>
+
+<script>
+import smallLogo from './assets/small-logo.svg';
+
+export default {
+  name: "header-main",
+  components: {
+    smallLogo,
+
+  },
+};
+</script>
+
+<style>
+svg {
+  width: 100%;
+  height: auto;
+
+}
+</style>
