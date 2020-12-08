29251926e0ff9b0efdb0fb23ee40e7d751f38029
Chloe Twilligear <chloelolee671@gmail.com>
2020-12-07 21:25:05 -0500

    header shit

--- vue/src/components/header.vue
+++ vue/src/components/header.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="header-main">
       <a href="#" class="cat-logo"><img  class="cat-logo" src="../assets/jpg/small-logo.jpg" /></a>
-      <img class="button fav" src="../assets/webp/favorites-button.webp"/>
-      <img class="button cats" src="../assets/webp/cats-button.webp"/>
-      <img class="button profile" src="../assets/webp/profile-button.webp"/>
-
+      <a href="#" class="button fav" > <img class="button fav" src="../assets/webp/favorites-button.webp"/></a>
+      <a href ="#" class ="button cats"><img class="button cats" src="../assets/webp/cats-button.webp"/> </a>
+      <a href ="#" class ="button profile"><img class="button profile" src="../assets/webp/profile-button.webp"/></a>
+      <form class="search"  > <input type="text" ><button type="submit" ><i class="material-icons" > search</i> </button> </form>
   </div>
 </template>
 
@@ -28,10 +28,17 @@ export default {
    grid-template-columns: 12% 20% 20% 20% 20% 8%;
    grid-template-areas: 
    "logo . . . . "
-  "logo fav cats profile test"
+  "logo fav cats profile search"
   "logo . . . . ";
+  justify-content: center;
+  justify-items: center;
 
  
+}
+.button {
+  /*align-self: center;*/
+  margin-left: 7%;
+  height: 80px;
 }
 
 .cat-logo {
@@ -45,21 +52,35 @@ export default {
 
 .cats {
   grid-area: cats;
+  margin-left: 9%;
 }
 
 .profile { 
   grid-area: profile;
+  margin-left: 3%;
+
 }
 
-.button {
-  /*align-self: center;*/
-  padding-left: 7%;
+.search{
+  grid-area: search;
 }
 
-
-.place {
-  grid-area: test;
+input, [type="button"], [type="reset"], [type="submit"] {
+    -webkit-appearance: button;
+    background-color: #EF2B76;
+    padding-left: 2rem;
+    padding-right: 2rem;
+    cursor: pointer;
+    color: white;
 }
+input, [type="text"] {
+    background-color: white;
+    padding-left: 2rem;
+    padding-right: 2rem;
+    cursor: pointer;
+    color: grey;
+}
+
 </style>
 
 
