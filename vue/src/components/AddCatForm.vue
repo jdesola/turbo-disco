303806c0cdbaf39a6026a8d1915b485e7b4d142e
Elaina <elainarusetsky@gmail.com>
2020-12-08 16:11:29 -0500

    form

--- vue/src/components/AddCatForm.vue
+++ vue/src/components/AddCatForm.vue
@@ -8,11 +8,12 @@
     
         <input type="text" id="age" name="age" placeholder="Age"/>
    
-        <input type="text" id="skills" name="skills" placeholder="Skills"/>
+        <input class="large-box" type="text" id="skills" name="skills" placeholder="Skills"/>
      
         <input type="text" id="color" name="color" placeholder="Color"/>
  
-        <select id="hair" name="hair" placeholder="Hair Length">
+        <select id="hair" name="hair">
+          <option value="" disabled selected>Hair Length</option>
           <option value="Long">Long</option>
            <option value="Short">Short</option>
             <option value="Hairless">Hairless</option>
@@ -20,7 +21,7 @@
      
         <input type="text" id="experience" name="experience" placeholder="Prior Experience"/>
       
-        <input type="text" id="description" name="description" placeholder="Description"/>
+        <input class="large-box" type="text" id="description" name="description" placeholder="Description"/>
         <div class="buttons">
         <button type="reset" value="reset">Reset</button>
         <button type="submit" value="submit">Submit</button>
@@ -42,11 +43,14 @@ export default {
 <style>
 .catFormContainer {
   display: flex;
- align-content: center;
+ /*align-content: center; */
+ width:50%;
+ margin: 0px auto;
+box-shadow: -2px 3px 15px 3px #575A8F;
 }
 
 .catImage {
-  width: 25%;
+  width:50%;
   height: auto;
   display: inline-block;
 }
@@ -55,8 +59,51 @@ export default {
   display: flex;
   flex-direction: column;
   height: max-content;
-  width: auto;
+  width: 50%;
   align-content: space-evenly;
   line-height: 24px;
+    color: #575A8F;
+    text-align: center;
+
+}
+.catForm input:first-child {
+margin-top: 25px;
+
+}
+
+.catForm > select{
+    color: #575a8f80;
+}
+
+.catForm > input, select{
+  height: 4%;
+   background-color: #D8D8D8;
+  border-radius: 14px;
+  color: #575A8F;
+  font-size: 24px;
+  font-weight: 500;
+  line-height: 30px;
+  font-family: Quicksand, sans-serif;
+  margin-bottom: 25px;
+}
+
+/* .large-box{
+  
+  padding:10px;
+  border-radius:10px;
+
+} */
+
+::-webkit-input-placeholder {
+   text-align: center;
+}
+:-moz-placeholder { /* Firefox 18- */
+   text-align: center;  
+}
+::-moz-placeholder {  /* Firefox 19+ */
+   text-align: center;  
+}
+:-ms-input-placeholder {  
+   text-align: center; 
 }
 </style>
