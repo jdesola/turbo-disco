c700bdd017be48fa68e20a0827fca91bd2f92014
Chloe Twilligear <chloelolee671@gmail.com>
2020-12-07 23:16:17 -0500

    header shit

--- vue/src/components/header.vue
+++ vue/src/components/header.vue
@@ -4,7 +4,13 @@
       <a href="#" class="button fav" > <img class="button fav" src="../assets/webp/favorites-button.webp"/></a>
       <a href ="#" class ="button cats"><img class="button cats" src="../assets/webp/cats-button.webp"/> </a>
       <a href ="#" class ="button profile"><img class="button profile" src="../assets/webp/profile-button.webp"/></a>
-      <form class="search"  > <input type="text" ><button type="submit" ><i class="material-icons" > search</i> </button> </form>
+      <div class="search-container">
+    <form action="/action_page.php">
+      <input type="text" name="search">
+      <button type="submit"><i class="material-icons"> search</i></button>
+    </form>
+  </div>
+      <!-- <form class="search"  > <input class="rectangle" type="text" ><button type="submit" ><i class="material-icons" > search</i> </button> </form> -->
   </div>
 </template>
 
@@ -26,14 +32,16 @@ export default {
   /*grid container*/
   display: grid;
    grid-template-columns: 12% 20% 20% 20% 20% 8%;
+   grid-template-rows: 22% 56% 22%;
    grid-template-areas: 
    "logo . . . . "
-  "logo fav cats profile search"
+  "logo fav cats profile search "
   "logo . . . . ";
-  justify-content: center;
   justify-items: center;
+  overflow: hidden;
+  border-bottom: 8px #575a8f solid;
+  border-radius: 7%;
 
- 
 }
 .button {
   /*align-self: center;*/
@@ -61,11 +69,60 @@ export default {
 
 }
 
-.search{
+.search-container{
   grid-area: search;
+  height: 76%;
+  width: 100%;
+  border-radius: 30px;
+  background-color: rgba(199,199,199,0.35);
+  box-shadow: -2px 3px 15px 3px rgba(0,0,0,0.5);
 }
 
-input, [type="button"], [type="reset"], [type="submit"] {
+.header-main .search-container {
+  float: right;
+  justify-self: center;
+  align-self: center;
+  margin-left: 20%;
+  margin-top: 2%;
+  
+}
+.header-main input[type=text] {
+  width: 68%;
+  margin-left: 7%;
+  margin-top: 7%;
+  font-size: 17px;
+  border: none;
+  color: #33a3f5;
+  background-color: rgba(199, 199, 199, 0);
+  outline: none;
+  font-family: 'Quicksand', sans-serif;
+
+}
+.header-main .search-container button {
+  float: right;
+  padding: 6px 10px;
+  margin-top: 8px;
+  margin-right: 16px;
+  font-size: 17px;
+  border: none;
+  cursor: pointer;
+  position: absolute;
+  align-self: center;
+  right: 5.2%;
+  background-color: rgba(199, 199, 199, 0);
+  color: #33a3f5;
+  border-left: #33a3f5 1.5px solid;
+
+}
+i{
+  height: 110%;
+  width: auto;
+}
+/* .header-main .search-container button:hover {
+  background: #ccc;
+} */
+
+/* input, [type="button"], [type="reset"], [type="submit"] {
     -webkit-appearance: button;
     background-color: #EF2B76;
     padding-left: 2rem;
@@ -73,13 +130,14 @@ input, [type="button"], [type="reset"], [type="submit"] {
     cursor: pointer;
     color: white;
 }
+
 input, [type="text"] {
     background-color: white;
     padding-left: 2rem;
     padding-right: 2rem;
     cursor: pointer;
     color: grey;
-}
+} */
 
 </style>
 
