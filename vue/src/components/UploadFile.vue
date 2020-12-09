db4d3f85f17c8a16a385f87d735da86f88c83333
Elaina <elainarusetsky@gmail.com>
2020-12-09 17:12:26 -0500

    Started photo uploader

---
+++ vue/src/components/UploadFile.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+    <input type="file" @change="onFileSelected">
+    <button @click="onUpload">Upload</button>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    name:'UploadPhoto',
+    data(){
+        return{
+            selectedFile: null
+        }
+    },
+
+methods:{
+    onFileSelected(event){
+        this.selectedFile = event.target.files[0]
+    },
+    onUpload(){
+        const fd = new FormData();
+        fd.append('image', this.selectedFile, this.selectedFile.name)
+        axios.post('CLOUD URL PLACEHOLDER', fd.then(res => {
+            console.log(res)
+        }))
+
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
