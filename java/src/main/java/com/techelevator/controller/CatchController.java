2af3b132418037e39e03d256a7e3212b1ef42bbc
Student <student@Techs-MacBook-Air.local>
2020-12-09 16:19:20 -0500

    stuff

--- java/src/main/java/com/techelevator/controller/CatchController.java
+++ java/src/main/java/com/techelevator/controller/CatchController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,14 +25,6 @@ public class CatchController {
 		this.catDao = catDao;
 	}
 	
-//	@RequestMapping (path= "/cats/{account_id}", method = RequestMethod.GET)
-//	public Account getAccountById(@PathVariable long account_id) throws AccountNotFoundException{
-//		
-//		if(accountDao.getAccountById(account_id) == null) {
-//			throw new AccountNotFoundException();
-//		}
-//		return accountDao.getAccountById(account_id);
-//	}
 	
 	@RequestMapping (path ="/cats", method=RequestMethod.GET)
 	public List<Cat> getCatz() {
@@ -46,11 +37,4 @@ public class CatchController {
 	public void cat (@RequestBody Cat cat) {
 		catDao.createCat(cat);
 	}
-	
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	@RequestMapping(path="/cats/{catId}", method=RequestMethod.DELETE)
-	public void delete(@PathVariable long catId) {
-		catDao.deleteCat(catId);
-	}
-	
 }
