65888e13ea328f0b91a930142c5390d34bd2dee5
Student <student@Techs-MacBook-Air.local>
2020-12-08 22:19:04 -0500

    Create Cat API

--- java/src/main/java/com/techelevator/controller/CatchController.java
+++ java/src/main/java/com/techelevator/controller/CatchController.java
@@ -2,9 +2,12 @@ package com.techelevator.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.techelevator.dao.CatDAO;
@@ -34,5 +37,10 @@ public class CatchController {
 		
 		return catDao.listCats();
 	}
-
+	
+	@ResponseStatus(HttpStatus.CREATED)
+	@RequestMapping(value="/cats/addCat", method = RequestMethod.POST)
+	public int cat (@RequestBody Cat cat) {
+		return catDao.createCat(cat);
+	}
 }
