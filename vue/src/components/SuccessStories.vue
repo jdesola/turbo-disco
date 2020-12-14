7110c8aa1d9649a5ccceae42488aafe123927809
Student <student@Techs-MacBook-Air.local>
2020-12-13 23:18:47 -0500

    Just Kidding

---
+++ vue/src/components/SuccessStories.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="SuccessStoryCard">
+      <v-card
+  elevation="2"
+>
+    <v-card-title> 
+
+    </v-card-title>
+</v-card>
+</div>
+
+
+    
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
