a815cb31654deae6a988ff39c79395cd89d8bc5a
Chloe Twilligear <chloelolee671@gmail.com>
2020-12-15 19:18:48 -0500

    new

--- vue/src/views/GalleryView.vue
+++ vue/src/views/GalleryView.vue
@@ -1,48 +1,76 @@
 <template>
-  <div class="GalleryPage">
-
-    <img class="image" v-bind:key ='image' v-for="(image, i) in images" :src="image" @click="onClick(i)"  @mouseover="hover = true"
-      @mouseleave="hover = false">
-    <span v-if="hover"> <br>This kitty is just so darn adorable! </span>
-    <gallery :images="images"  @close="index = null"></gallery>
-
- </div>
+  <v-container>
+    <v-row>
+      <v-col col="12">
+        <v-carousel>
+          <v-carousel-item
+            v-for="(item,i) in items"
+            :key="i"
+            :src="item.src"
+            reverse-transition="fade-transition"
+            transition="fade-transition"
+          ></v-carousel-item>
+        </v-carousel>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
+    <!-- <img class="image" v-bind:key ='image' v-for="(image, i) in images" :src="image" @click="onClick(i)"  @mouseover="hover = true"
+      @mouseleave="hover = false">
+    <span v-if="hover"> <br>This kitty is just so darn adorable! </span>
+    <gallery :images="images"  @close="index = null"></gallery> -->
+
+
 <script>
-import Gallery from '../components/Gallery'
-
 export default {
-    name: 'galleryPage',
-    components:{
-        Gallery
+  name: "galleryPage",
+  methods: {
+    onClick(i) {
+      this.index = i;
     },
-    methods: {
-      onClick(i) {
-        this.index = i;
-      }
-    },
-      data: function() {
-        return {
-            hover:false,
-            images: [
-                'https://placekitten.com/801/800',
-                'https://placekitten.com/802/800',
-                'https://placekitten.com/803/800',
-                'https://placekitten.com/804/800',
-                'https://placekitten.com/805/800',
-                'https://placekitten.com/806/800',
-                'https://placekitten.com/807/800',
-                'https://placekitten.com/808/800',
-                'https://placekitten.com/809/800',
-                'https://placekitten.com/810/800',
-            ],
-            index: 0
-        };
-    }
-
-}
-
+  },
+  data: function () {
+    return {
+      items: [
+        {
+          src: "https://placekitten.com/801/800",
+        },
+        {
+          src: "https://placekitten.com/802/800",
+        },
+        {
+          src: 'https://placekitten.com/803/800',
+        },
+        {
+          src: "https://placekitten.com/804/800",
+        },
+        {
+          src: "https://placekitten.com/805/800",
+        },
+        {
+          src: "https://placekitten.com/806/800",
+        },
+        {
+          src: "https://placekitten.com/807/800",
+        },
+      ],
+      images: [
+        "https://placekitten.com/801/800",
+        "https://placekitten.com/802/800",
+        "https://placekitten.com/803/800",
+        "https://placekitten.com/804/800",
+        "https://placekitten.com/805/800",
+        "https://placekitten.com/806/800",
+        "https://placekitten.com/807/800",
+        "https://placekitten.com/808/800",
+        "https://placekitten.com/809/800",
+        "https://placekitten.com/810/800",
+      ],
+      index: null,
+    };
+  },
+};
 </script>
 
 <style scoped>
@@ -58,6 +86,5 @@ body {
   margin: 10px;
   border-radius: 3px;
 }
-
 </style>
 <!-- -->
