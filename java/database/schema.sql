877c6f5f9bcfd96f01bff85a96ef6084ec1fd732
Chloe Twilligear <chloelolee671@gmail.com>
2020-12-16 22:30:01 -0500

    updated schema

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -106,17 +106,29 @@ insert into catch_cats values (default, 'Paw Jovi', 45, 'Singing, being adorable
 insert into catch_cats values (default, 'Mew.js', 2, 'Interior design, agility', 'Long', 12,'Programmer', 'Mew.js is very good at making the things behind the scenes look pretty up front', 'White, brown, gray', false, 'cat_using_computer.jpg', true, 2,4,3,3,1);
 insert into catch_cats values (default, 'Kitty Purry', 3, 'Pretending to sing', 'Long', 43, 'Singer', 'Starting catfights', 'White', false, 'Katy_perry_and_kitty_purry.jpg', true, 1,1,1,1,1);
 insert into catch_cats values (default, 'Mammal', 1, 'Existing, eating, sleeping', 'Hairless', 0, 'N/A', 'Likes to snuggle, but is so unattractive people generally avoid him', 'Flesh', false, 'hairlesscat.jpg',true, 2,3,4,2,2);
---need homes
+--need homes--
 insert into catch_cats values (default, 'Pancake', 12, 'Carboloading, getting beefcake', 'Short', 4, 'Sommelier', 'Has an appreciation for the finer things in life', 'Orange', false, 'image.png', false, 3, 3, 3, 5, 3);
 insert into catch_cats values (default, 'Genevieve', 2, 'Belly flops, marathon training', 'Short', 17, 'Bodybuilder', 'Genevieve is a part-time fitness model and loves pina coladas and long walks on the beach', 'Multi', false, 'bellycat.jpg', false, 4, 3, 4, 5, 2);
 insert into catch_cats values (default, 'Izzy', 1, 'Costume design, photography, Adobe Suite', 'Short', 2, 'Seamstress', 'Izzy likes to cosplay in her spare time. Once she is bigger than the typical mouse, she looks forward to chasing them out of the house', 'Orange',false, 'leia.jpg',false, 1, 4,3,4,1);
+insert into catch_cats values (default, 'Brian', 20, 'Avid guitar player and java development wizard', 'Short', 24, 'Java Development Instructor', 'Brian likes to do Bee cosplay and is a Bob Seger fanatic', 'Grey',false, 'image(1).png',false, 3, 4,3,4,3);
+insert into catch_cats values (default, 'Rachelle', 14, 'The Lady of Perpetual Breaks', 'Short', 2, 'Assistant Java Instructor', 'She loves her students, her kittens, and her dog Turkey', 'Grey/White', false, 'image(2).png',false, 2, 4,2,4,2);
+insert into catch_cats values (default, 'Chloe', 2, 'Drawing and sleeping', 'Short', 3, 'Ice Cream Scooper', 'Playing minecraft, draw, and thats about it', 'White/Brown', false, 'image(3).png',false, 2, 3,2,4,1);
+insert into catch_cats values (default, 'Olivia', 3, 'Photgraphy, website development ', 'Short', 7, 'Optician', 'Likes to go to South Carolina, drives fast and take pictures', 'Grey', false, 'image(4).png',false, 2, 4,5,3,2);
+insert into catch_cats values (default, 'Josh', 5, 'Building PCs', 'Short', 9, 'Project Manager', 'Loves to build PCs and cosplaying as his Spidey-sona', 'White/Orange', false, 'josh.jpg',false, 3, 3,2,4,3);
+insert into catch_cats values (default, 'Elaina', 5, 'Makeup and Therapy for friends', 'Short', 6, 'Roofing Contract Specialist', 'Elaina loves to play with makeup in her spare time, and playing Dont Starve', 'Black', false, 'catsona.png',false, 3, 3,5,4,1);
+insert into catch_cats values (default, 'Kevin', 9, 'DESTRUCTION', 'Short', 9999, 'DESTORYER OF WORLDS', 'HATES RAINBOWS AND JOY', 'Black/Brown', false, 'image.png',false, 5, 5,5,5,1);
+insert into catch_cats values (default, 'Winston', 1, 'Being adorable', 'Short', 1, 'Crown Prince', 'Winston loves to lick everyone on the face', 'Mix', false, 'teacup.jpg',false, 1, 1,1,1,3);
+insert into catch_cats values (default, 'Sebastian', 3, 'Being smart', 'Hairless', 6, 'Head Scientist at NASA', 'Likes to read scientific articles and observing stars in the visible universe ', 'Bald', false, 'hairlesscat.jpg',false, 5, 5,5,5,2);
 
 
-insert into success_stories values (default, 2, 'Laurence of Olivier', '12/24/2019', 'I adopted Keamew in the middle of a knife fight. The other guy pulled a gun on me (totes breaking the rules) and Keamew jumped out to take the bullet. He survived and we’ve been buddies ever since. He will be the best man at my wedding next week!');
-insert into success_stories values (default, 3, 'Cindy Lauper', '10/31/2020', 'Nosferatmew is weird because he only comes out in the evenings, but he definitely gets the job done! We haven’t seen a single live mouse since we adopted him, just their sad, shriveled little corpses. We make our daughter bury them out back to build character.');
+
+--success stories---
+
+insert into success_stories values (default, 2, 'Laurence of Olivier', '12/24/2019', 'I adopted Keamew in the middle of a knife fight. The other guy pulled a gun on me (totes breaking the rules) and Keamew jumped out to take the bullet. He survived and weï¿½ve been buddies ever since. He will be the best man at my wedding next week!');
+insert into success_stories values (default, 3, 'Cindy Lauper', '10/31/2020', 'Nosferatmew is weird because he only comes out in the evenings, but he definitely gets the job done! We havenï¿½t seen a single live mouse since we adopted him, just their sad, shriveled little corpses. We make our daughter bury them out back to build character.');
 insert into success_stories values (default, 4, 'Bob Belcher', '11/27/2020', 'It''s my life / It''s meow or never / I ain''t gonna live furever');
 insert into success_stories values (default, 5, 'Brian Lauvray', '12/18/2020', 'Props to the Catch team. Mew.js has become a major component in my life!');
-insert into success_stories values (default, 6, 'Rachelle Raugh', '12/18/2020', 'Our experience with Catch has been nothing short of phenomenal. We love our Kitty Purry! They say she used to be a singer, but we just don’t hear it. In fact, whenever she meows, it sounds kind of like when you shove too many potato peels down a garbage disposal. Oh well, at least she is pretty to look at!');
+insert into success_stories values (default, 6, 'Rachelle Raugh', '12/18/2020', 'Our experience with Catch has been nothing short of phenomenal. We love our Kitty Purry! They say she used to be a singer, but we just donï¿½t hear it. In fact, whenever she meows, it sounds kind of like when you shove too many potato peels down a garbage disposal. Oh well, at least she is pretty to look at!');
 insert into success_stories values (default, 7, 'Human Being', '08/13/2020', 'We are pleased with Catch. They have provided a functional cat devoid of defects.');
 
 COMMIT TRANSACTION;
